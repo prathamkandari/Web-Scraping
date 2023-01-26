57d0be18002dcc9651c4f198d0797abdf075c18c
prathamkandari <prathamkandari123@gmail.com>
2023-01-26 12:46:39 +0530

    Scraping

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.upescsa.in/"
+
+# For getting the HTMl
+r = requests.get(url)
+htmlContent = r.content
+# print(htmlContent)
+# HTML parser
+soup = BeautifulSoup(htmlContent, 'html.parser')
+# print(soup.prettify)
+
+title=soup.title
+# print(title)
+
+
+# Printing the first para of the upes csa
+
+print(soup.find_all("p", class_="content"))

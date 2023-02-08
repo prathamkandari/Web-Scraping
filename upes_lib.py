96f7d45789214efde3ae4180ee2edf3bdcde9f6d
prathamkandari <prathamkandari123@gmail.com>
2023-02-08 23:29:06 +0530

    New Update

--- upes_lib.py
+++ upes_lib.py
@@ -16,23 +16,18 @@ soup = BeautifulSoup(htmlContent, 'html.parser')
 # lists = soup.findAll('a',class_="LINK")
 
 list2=[]
-list3=[]
-list4=[]
 
 for link in soup.find_all('a'):
     list2.append(link.get('href'))
 
 del list2[0:9]
 del list2[0]
-print(list2)
 
-
-
-# fields= ['LINKS']
-# with open('CSV File.csv', 'w+') as f:
-    # csv_writer = csv.writer(f)
-    # csv_writer.writerow(fields)
-    # csv_writer.writerow(list2)
+fields= ['LINKS']
+with open('CSV File.csv', 'w+') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(fields)
+    csv_writer.writerow(list2)
 # for line in lists:
     # for i in line:
         # print(i)
@@ -42,8 +37,7 @@ print(list2)
 #     print(list2[i])
 
 
-# df = pd.DataFrame(list2, columns=['Links'])
-# print(df)
-# df.to_csv('CSV File.csv')
+df = pd.DataFrame(list2, columns=['Links'])
+df.to_csv('CSV File.csv')
 
 
